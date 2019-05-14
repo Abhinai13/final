df1eb24783454c3b1744110d09612e9feea29985
Abhinai <Abhinai13rules@gmail.com>
2019-05-14 18:14:28 -0400

    Added all routes, controllers and view blades. Login with seeded user, and homepage with questions displayed via pager now work

---
+++ resources/views/question.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Question</div>
+
+                    <div class="card-body">
+
+                        {{$question->body}}
+                    </div>
+                    <div class="card-footer">
+                        <a class="btn btn-primary float-right"
+                           href="{{ route('question.edit',['id'=> $question->id])}}">
+                            Edit Question
+                        </a>
+
+                        {{ Form::open(['method'  => 'DELETE', 'route' => ['question.destroy', $question->id]])}}
+                        <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
+                        </button>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header"><a class="btn btn-primary float-left"
+                                                href="{{ route('answer.create', ['question_id'=> $question->id])}}">
+                            Answer Question
+                        </a></div>
+
+                    <div class="card-body">
+                        @forelse($question->answer as $answer)
+                            <div class="card">
+                                <div class="card-body">{{$answer->body}}</div>
+                                <div class="card-footer">
+                                    <a class="btn btn-success"
+                                       href="{{ route('vote.downvote', ['answer_id'=> $answer->id]) }}">
+                                        UP
+                                    </a>
+                                    <a class="btn btn-danger"
+                                       href="{{ route('vote.downvote', ['answer_id'=> $answer->id]) }}">
+                                        Down
+                                    </a>
+                                    <a class="btn btn-primary float-right"
+                                       href="{{ route('answer.show', ['question_id'=> $question->id,'answer_id' => $answer->id]) }}">
+                                        View
+                                    </a>
+
+                                </div>
+                            </div>
+                        @empty
+                            <div class="card">
+
+                                <div class="card-body"> No Answers</div>
+                            </div>
+                        @endforelse
+
+
+                    </div>
+                </div>
+            </div>
+@endsection
