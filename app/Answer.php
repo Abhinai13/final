d2f2fabbb6e63b130a1c79cb91d7658d39d08f62
Abhinai <Abhinai13rules@gmail.com>
2019-05-14 17:42:58 -0400

    Finished up answer functionality so answers can now be seeded and model is complete with relationships to user and questions

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    protected $fillable = ['body'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function question()
+    {
+        return $this->belongsTo('App\Question');
+    }
 }
