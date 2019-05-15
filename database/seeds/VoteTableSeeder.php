61cb23510052cdfc58907635d09c3fdfd86f0115
Abhinai <Abhinai13rules@gmail.com>
2019-05-14 22:01:36 -0400

    Created VoteTableSeeder and Updated Database Seeder

---
+++ database/seeds/VoteTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Answer;
+use App\Question;
+
+use Illuminate\Database\Seeder;
+
+class VoteTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = App\User::all();
+        for ($i = 1; $i <= 6; $i++) {
+            $users->each(function ($user) {
+                $answer = App\Answer::inRandomOrder()->first();
+                $vote = factory(\App\Vote::class)->make();
+                $vote->user()->associate($user);
+                $vote->answer()->associate($answer);
+                $vote->save();
+            });
+        }
+    }
+}
